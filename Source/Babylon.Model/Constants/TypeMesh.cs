619bb6cf81d64f4313541034f78b72d70ede84d6
Andrii Kyrstiuk <akyrstiuk@masao.eu>
2022-04-01 17:09:14 +0300

    Added class creator for box mesh

---
+++ Source/Babylon.Model/Constants/TypeMesh.cs
@@ -0,0 +1,18 @@
+﻿namespace Babylon.Model.Constants
+{
+    public class TypeMesh
+    {
+        public enum Mesh
+        {
+            Box,
+            Torus,
+            Cylinder,
+            Sphere
+        }
+        //public const string
+        //    Box = "Box",
+        //    Torus = "Torus",
+        //    Cylinder = "Cylinder",
+        //    Sphere = "Sphere";
+    }
+}
---
+++ Source/Babylon.Shared/MeshCreator/BoxCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using Babylon.Blazor;
+using Babylon.Blazor.Babylon;
+using Babylon.Blazor.Babylon.Parameters; 
+using Babylon.Blazor.Models.ServiceContracts;
+
+namespace Babylon.Shared.MeshCreator
+{
+    public class BoxCreator:ICreatorMesh
+    {
+        private readonly IBabylonInstance _babylonInstance;
+
+        public Scene Scene { get; }
+
+        public BoxCreator(IBabylonInstance babylonInstance, Scene scene)
+        {
+            _babylonInstance = babylonInstance;
+            Scene = scene;
+        }
+        
+        public Task<Mesh> CreateMesh()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task AddBox(string name)
+        {
+            BoxOptions.FaceColorsObj boxColors = await _babylonInstance.CreateFaceColors(Color.Blue);
+            Options boxOptions = new BoxOptions { Height = 2, Width = 5.5, Depth = 0.5, FaceColors = boxColors };
+            MeshParameters boxParameters = new MeshParameters(_babylonInstance) { Options = boxOptions };
+            await boxParameters.SetPosition(10, 0, 0);
+            await Scene.CreateBox(name, boxParameters);
+        }
+    }
+}
